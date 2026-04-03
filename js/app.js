21d077d1903ed87af3f25caa53882c4c6f204edb
unknown <somegetiye@gmail.com>
2026-04-03 12:24:21 -0700

    deleting tasks

--- js/app.js
+++ js/app.js
@@ -4,14 +4,40 @@ const remove = document.querySelector(".btn--remove");
 const taskList = document.querySelector(".task-list");
 const miniNav = document.querySelector(".mini--nav");
 
-const taskes = [];
+let taskes = [];
+const completedTask = [];
 taskList.innerHTML = "";
 
-if (taskes.length === 0) {
-  const noItem = document.createElement("p");
-  noItem.textContent = "There is no Task to show!!";
-  taskList.appendChild(noItem);
+/////Adding the task to the DOM
+
+function renderTaskList(taskes) {
+  taskList.innerHTML = "";
+
+  taskes?.forEach((task) => {
+    const li = document.createElement("li");
+    li.className = "task";
+    li.dataset.id = task.id;
+
+    li.innerHTML = `
+    <div class='task--wrapper'>
+    <input type="checkbox"class='check-box' />
+    <div class="task--content">
+
+        <p class="task-desc">${task.title}</p>
+        <time>${task.Timestamp}</time>
+
+        </div>
+        </div>
+        <button class='btn--remove'>
+        <ion-icon name="trash-outline" class='remove' role='button'></ion-icon></button>
+     
+    `;
+
+    taskList.appendChild(li);
+  });
 }
+
+////Adding new Task to array of taskes
 function handleAddTask(e) {
   e.preventDefault();
 
@@ -25,37 +51,20 @@ function handleAddTask(e) {
       hour: "numeric",
       minute: "numeric",
     }).format(Date.now()),
+    completed: false,
   };
 
-  taskes.push(newTask);
-  // console.log(newTask);
-  taskList.innerHTML = "";
+  if (newTask.completed === true) {
+    completedTask.push(newTask);
+  }
 
-  taskes.forEach((task) => {
-    const li = document.createElement("li");
-    li.className = "task";
+  taskes.unshift(newTask);
 
-    li.innerHTML = `
-    <div class='task--wrapper'>
-    <input type="checkbox" />
-    <div class="task--content">
-
-        <p>${task.title}</p>
-        <time>${task.Timestamp}</time>
-
-        </div>
-        </div>
-        <button class='btn--remove'>
-        <ion-icon name="trash-outline"></ion-icon></button>
-     
-    `;
-
-    taskList.appendChild(li);
-  });
+  renderTaskList(taskes);
 
   taskInput.value = "";
 }
-addBtn.addEventListener("click", handleAddTask);
+addBtn.addEventListener("click", (e) => handleAddTask(e));
 
 /////For Toggling mini nav
 
@@ -71,16 +80,59 @@ miniNav.addEventListener("click", function (e) {
 
   link.classList.add("nav--active");
 });
-function handleDelet(id) {
-  console.log(id);
-  console.log("this was deleted");
-  taskes.filter((task) => task.id !== id);
-}
 
 taskList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn--remove")) {
-    console.log("Delete button clicked");
-  }
+  const checkbox = e.target.closest("input[type='checkbox']");
+  if (!checkbox) return;
 
-  console.log("the list was clicked");
+  const li = checkbox.closest(".task");
+  const id = Number(li.dataset.id);
 });
+
+///// Handling the line througn property
+taskList.addEventListener("change", function (e) {
+  if (e.target.classList.contains("check-box")) {
+    const isChecked = e.target.checked;
+    const task = e.target.closest(".task");
+    const taskDesc = task.querySelector(".task-desc");
+
+    const id = +task.dataset.id;
+
+    const completArray = taskes.find((task) => task.id == id);
+    console.log("INITIAL:", completedTask);
+    if (isChecked) {
+      taskDesc.style.textDecoration = "line-through";
+      completedTask.unshift(completArray);
+    } else {
+      taskDesc.style.textDecoration = "none";
+
+      const index = completedTask.findIndex((t) => t.id == id);
+      if (index !== -1) {
+        completedTask.splice(index, 1);
+      }
+    }
+  }
+  // console.log(completedTask);
+});
+
+/////Handling the delete task
+
+// function handleDelete(e) {}
+taskList.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("remove")) return;
+  const task = e.target.closest(".task");
+  const id = +task.dataset.id;
+
+  if (e.target.classList.contains("remove")) {
+    taskes = taskes.filter((task) => task.id !== id);
+    renderTaskList(taskes);
+    console.log(taskes);
+  }
+});
+if (taskes.length === 0) {
+  const noItem = document.createElement("p");
+  noItem.textContent = "There is no Task to show!!";
+  noItem.style.fontSize = "18px";
+  noItem.style.paddingTop = "20px";
+  taskList.appendChild(noItem);
+}
